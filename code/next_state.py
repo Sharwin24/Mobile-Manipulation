3a4f164e1335dbdaf87a4409a6aa181c6dcef464
Sharwin24 <sharwin24@gmail.com>
2024-12-01 20:31:08 -0600

    implemented function without odometry

--- code/next_state.py
+++ code/next_state.py
@@ -20,5 +20,31 @@ def next_state(
     # new arm joint angles = (old arm joint angles) + (joint speeds) * dt
     # new wheel angles = (old wheel angles) + (wheel speeds) * dt
     # new chassis configuration is obtained from odometry, as described in Chapter 13.4
-
     # The output is a robot_state vector after the timestep
+
+    chassis_config = robot_state[:3]  # x, y, theta
+    arm_config = robot_state[3:8]
+    wheel_config = robot_state[8:]
+    wheel_speeds = robot_speeds[:4]
+    arm_speeds = robot_speeds[4:]
+
+    # limit the speeds to the max allowed (negative and positive)
+    if max_wheel_motor_speed:
+        wheel_speeds = np.clip(
+            wheel_speeds, -max_wheel_motor_speed, max_wheel_motor_speed
+        )
+    if max_arm_motor_speed:
+        arm_speeds = np.clip(
+            arm_speeds, -max_arm_motor_speed, max_arm_motor_speed
+        )
+
+    new_arm_config = arm_config + arm_speeds * dt
+    new_wheel_config = wheel_config + wheel_speeds * dt
+
+    # TODO: Odometry
+    new_chassis_config = chassis_config
+
+    new_robot_state = np.concatenate(
+        [new_chassis_config, new_arm_config, new_wheel_config]
+    )
+    return new_robot_state
