b4416275fcaa232edac4a7f72cca8cf55d94e130
Sharwin Patil <sharwin24@gmail.com>
2024-12-11 00:46:34 -0600

    cleaning up comments

--- code/next_state.py
+++ code/next_state.py
@@ -41,7 +41,7 @@ def next_state(robot_state, robot_speeds, dt: float,
                max_arm_motor_speed: float, max_wheel_motor_speed: float) -> np.array:
     """
     Given a robot state and the robot speeds, compute the next state after a timestep.
-    Also clip the speeds to the maximum allowed values.
+    Also clip the speeds to the maximum allowed values. This function uses simple Euler integration.
 
     Each robot state is a 13x1 vector:
     - 3 for chassis config [phi, x, y]
@@ -62,22 +62,9 @@ def next_state(robot_state, robot_speeds, dt: float,
     Returns:
         np.array: The new robot state after the timestep
     """
-    # robot_state is a 13x1 vector
-    # 3 for chassis config, 5 for arm, 4 for wheel angles, 1 for gripper state
-    # robot_speeds is a 9x1 vector
-    # 4 wheel speeds, 5 arm speeds
-    # dt is the timestep
-    # max_arm_motor_speed is a scalar that limits the arm motor speed
-    # max_wheel_motor_speed is a scalar that limits the wheel motor speed
 
-    # The function NextState is based on a simple first-order Euler step, i.e.,
-    # new arm joint angles = (old arm joint angles) + (joint speeds) * dt
-    # new wheel angles = (old wheel angles) + (wheel speeds) * dt
-    # new chassis configuration is obtained from odometry, as described in Chapter 13.4
-    # The output is a robot_state vector after the timestep
-
-    chassis_config = robot_state[:3]  # phi, x, y
-    arm_config = robot_state[3:8]
+    chassis_config = robot_state[:3]  # [phi, x, y]
+    arm_config = robot_state[3:8]  # [theta1, theta2, theta3, theta4, theta5]
     wheel_config = robot_state[8:12]
     wheel_speeds = robot_speeds[:4]
     arm_speeds = robot_speeds[4:]
@@ -118,7 +105,7 @@ def simulate(
         current_state = states[-1]  # Latest state
         new_state = next_state(
             current_state,
-            np.concatenate([arm_speeds, wheel_speeds]),
+            np.concatenate([wheel_speeds, arm_speeds]),
             dt
         )
         states.append(new_state)
